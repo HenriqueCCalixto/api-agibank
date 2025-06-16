f8324793fc67f78b82048db631a5b84b17097679
Henrique C. <143053465+HenriqueCCalixto@users.noreply.github.com>
2025-06-15 22:19:58 -0300

    feat(cliente): add resilience retry and circuit

--- api/src/main/java/com/seguro/api/adapter/client/ClienteApiClient.java
+++ api/src/main/java/com/seguro/api/adapter/client/ClienteApiClient.java
@@ -1,6 +1,10 @@
 package com.seguro.api.adapter.client;
 
 import com.seguro.api.adapter.client.dto.ClienteDTO;
+import com.seguro.api.domain.exceptions.ClienteNaoEncontradoException;
+import com.seguro.api.domain.exceptions.ServiceNotFound;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -14,12 +18,19 @@ public class ClienteApiClient {
         this.restTemplate = restTemplate;
     }
 
+    @Retry(name = "buscarCliente")
+    @CircuitBreaker(name = "buscarCliente", fallbackMethod = "fallBackBuscarCliente")
     public ClienteDTO buscarClientePorId(Long idCliente) {
-        try {
-            String url = "http://localhost:8080/api/clientes/" + idCliente;
-            return restTemplate.getForObject(url, ClienteDTO.class);
-        } catch (HttpClientErrorException.NotFound e) {
-            throw new RuntimeException("Cliente não encontrado na API de Cadastro.");
-        }
+        String url = "http://localhost:8080/api/clientes/" + idCliente;
+        return restTemplate.getForObject(url, ClienteDTO.class);
     }
+
+    public ClienteDTO fallBackBuscarCliente(Long idCliente, Throwable ex) {
+        if (ex instanceof HttpClientErrorException.NotFound) {
+            throw new ClienteNaoEncontradoException("Cliente não encontrado.");
+        }
+        throw new ServiceNotFound("Serviço de Cliente está temporariamente indisponível.");
+    }
+
+
 }
