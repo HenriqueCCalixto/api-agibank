a8bb16f6fb47dbcae2cde3ff916209c64f4c9567
Henrique C. <143053465+HenriqueCCalixto@users.noreply.github.com>
2025-06-15 23:18:32 -0300

    feat(name): change name

--- api/src/main/java/com/seguro/api/application/service/cliente/BuscarClienteApiService.java
+++ api/src/main/java/com/seguro/api/application/service/cliente/BuscarClienteApiService.java
@@ -1,16 +1,16 @@
 package com.seguro.api.application.service.cliente;
 
-import com.seguro.api.adapter.client.ClienteApiClient;
-import com.seguro.api.adapter.client.dto.ClienteDTO;
+import com.seguro.api.adapter.client.cliente.ClienteAPIClient;
+import com.seguro.api.adapter.client.dto.cliente.ClienteDTO;
 import com.seguro.api.application.usecases.cliente.BuscarClienteUseCase;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BuscarClienteApiService implements BuscarClienteUseCase {
 
-    private final ClienteApiClient clienteApiClient;
+    private final ClienteAPIClient clienteApiClient;
 
-    public BuscarClienteApiService(ClienteApiClient clienteApiClient) {
+    public BuscarClienteApiService(ClienteAPIClient clienteApiClient) {
         this.clienteApiClient = clienteApiClient;
     }
 
--- api/src/main/java/com/seguro/api/application/usecases/cliente/BuscarClienteUseCase.java
+++ api/src/main/java/com/seguro/api/application/usecases/cliente/BuscarClienteUseCase.java
@@ -1,6 +1,6 @@
 package com.seguro.api.application.usecases.cliente;
 
-import com.seguro.api.adapter.client.dto.ClienteDTO;
+import com.seguro.api.adapter.client.dto.cliente.ClienteDTO;
 
 public interface BuscarClienteUseCase {
     ClienteDTO execute(Long idCliente);
